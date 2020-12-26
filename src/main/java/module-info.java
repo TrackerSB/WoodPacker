363e4b8c2d58ab5c196b569b1d1fcaf8b412d209
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-12-26 10:51:41 +0100

    Add prototype for resizable canvas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module bayern.steinbrecher.WoodPacker {
     requires bayern.steinbrecher.ScreenSwitcher;
+    requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
     requires org.jetbrains.annotations;
 
     opens bayern.steinbrecher.woodPacker to javafx.graphics;
     opens bayern.steinbrecher.woodPacker.screens to javafx.fxml;
+    opens bayern.steinbrecher.woodPacker.utility to javafx.fxml;
 }
