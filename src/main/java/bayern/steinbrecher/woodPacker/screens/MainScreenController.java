7d8516f5c9b7745e48c7597217c4f4e5f8d5ab20
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-12-26 10:56:07 +0100

    Add initial layout for packing plan visualization

--- src/main/java/bayern/steinbrecher/woodPacker/screens/MainScreenController.java
+++ src/main/java/bayern/steinbrecher/woodPacker/screens/MainScreenController.java
@@ -1,6 +1,22 @@
 package bayern.steinbrecher.woodPacker.screens;
 
 import bayern.steinbrecher.screenSwitcher.ScreenController;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ListView;
+import javafx.scene.paint.Color;
 
 public class MainScreenController extends ScreenController {
+    @FXML
+    private ListView<?> requiredBoardList;
+    @FXML
+    private Canvas visualBoardPlan;
+
+    @FXML
+    private void initialize(){
+        GraphicsContext graphicsContext = visualBoardPlan.getGraphicsContext2D();
+        graphicsContext.setFill(Color.GHOSTWHITE);
+        graphicsContext.fillRect(0, 0, visualBoardPlan.getWidth(), visualBoardPlan.getHeight());
+    }
 }
