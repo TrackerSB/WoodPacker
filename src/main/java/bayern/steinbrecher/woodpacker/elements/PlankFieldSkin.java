6b75db2f746dc8d3af2af548ae51733d545d6673
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-12-30 11:15:41 +0100

    PlankField: Automatically determine grain direction

--- src/main/java/bayern/steinbrecher/woodpacker/elements/PlankFieldSkin.java
+++ src/main/java/bayern/steinbrecher/woodpacker/elements/PlankFieldSkin.java
@@ -2,61 +2,117 @@ package bayern.steinbrecher.woodpacker.elements;
 
 import bayern.steinbrecher.checkedElements.spinner.CheckedIntegerSpinner;
 import bayern.steinbrecher.woodpacker.WoodPacker;
+import bayern.steinbrecher.woodpacker.data.PlankGrainDirection;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.SkinBase;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.util.function.BiConsumer;
 
 /**
  * @author Stefan Huber
  * @since 0.1
  */
 public class PlankFieldSkin extends SkinBase<PlankField> {
-    protected PlankFieldSkin(PlankField control) {
-        super(control);
+    private final BooleanProperty indicatorChangedByUser = new SimpleBooleanProperty(false);
 
-        CheckedIntegerSpinner widthField = new CheckedIntegerSpinner(1, Integer.MAX_VALUE, 1000, 1);
-        // widthField.setStyle("-fx-background-image: url('/bayern/steinbrecher/woodpacker/elements/plankWidth.png');"); // FIXME Icon not showing up
-        widthField.setPromptText(WoodPacker.LANGUAGE_BUNDLE.getString("width"));
-        widthField.setEditable(true);
-        control.plankWidthProperty()
-                .bind(widthField.valueProperty());
-        control.plankWidthProperty()
-                .addListener((ob, oldWidth, newWidth) -> {
-                    widthField.getEditor()
-                            .setText(String.valueOf(newWidth));
-                });
-
-        CheckedIntegerSpinner heightField = new CheckedIntegerSpinner(1, Integer.MAX_VALUE, 1000, 1);
-        // heightField.setStyle("-fx-background-image: url('/bayern/steinbrecher/woodpacker/elements/plankHeight.png');"); // FIXME Icon not showing up
-        heightField.setPromptText(WoodPacker.LANGUAGE_BUNDLE.getString("height"));
-        heightField.setEditable(true);
-        control.plankHeightProperty()
-                .bind(heightField.valueProperty());
-        control.plankHeightProperty()
-                .addListener((ob, oldHeight, newHeight) -> {
-                    heightField.getEditor()
-                            .setText(String.valueOf(newHeight));
-                });
-
+    private Node createGrainIndicator(PlankField control) {
         PlankGrainDirectionIndicator indicator = new PlankGrainDirectionIndicator();
         control.grainDirectionProperty()
                 .bindBidirectional(indicator.valueProperty());
 
+        Rectangle autoStateBackground = new Rectangle();
+        autoStateBackground.setFill(Color.rgb(255, 255, 255, 0.5));
+        autoStateBackground.widthProperty()
+                .bind(indicator.widthProperty());
+        autoStateBackground.heightProperty()
+                .bind(indicator.heightProperty());
+        autoStateBackground.visibleProperty()
+                .bind(indicatorChangedByUser.not());
+
+        Text autoStateText = new Text("A");
+        autoStateText.visibleProperty()
+                .bind(indicatorChangedByUser.not());
+
+        StackPane indicatorNode = new StackPane(indicator, autoStateBackground, autoStateText);
+        indicatorNode.setOnMouseClicked(mevt -> {
+            indicatorChangedByUser.set(true);
+            indicator.fireEvent(mevt); // FIXME This event does not reach the underlying button
+        });
+
+        return indicatorNode;
+    }
+
+    /**
+     * @param forWidth {@code true} for width field; {@code false} for height field
+     */
+    private Node createLengthField(PlankField control, boolean forWidth) {
+        CheckedIntegerSpinner lengthField = new CheckedIntegerSpinner(1, Integer.MAX_VALUE, 1000, 1);
+        // @formatter: off
+        // widthField.setStyle("-fx-background-image: url('/bayern/steinbrecher/woodpacker/elements/plankWidth.png');"); // FIXME Icon not showing up
+        // heightField.setStyle("-fx-background-image: url('/bayern/steinbrecher/woodpacker/elements/plankHeight.png');"); // FIXME Icon not showing up
+        // @formatter: on
+        lengthField.setPromptText(WoodPacker.LANGUAGE_BUNDLE.getString(forWidth ? "width" : "height"));
+        lengthField.setEditable(true);
+        IntegerProperty lengthProperty = forWidth ? control.plankWidthProperty() : control.plankHeightProperty();
+        lengthProperty.bind(lengthField.valueProperty());
+        lengthProperty.addListener((ob, oldLength, newLength) -> {
+            lengthField.getEditor()
+                    .setText(String.valueOf(newLength));
+        });
+
+        String externalIconPath = getClass()
+                .getResource(forWidth ? "plankWidth.png" : "plankHeight.png")
+                .toExternalForm();
+        ImageView lengthIcon = new ImageView(externalIconPath);
+        lengthIcon.setPreserveRatio(true);
+        lengthIcon.fitHeightProperty()
+                .bind(lengthField.heightProperty());
+
+        HBox content = new HBox(lengthIcon, lengthField);
+        content.setAlignment(Pos.CENTER_LEFT);
+        return content;
+    }
+
+    protected PlankFieldSkin(PlankField control) {
+        super(control);
+
+        BiConsumer<Integer, Integer> autoUpdateIndicator = (plankWidth, plankHeight) -> {
+            if (!indicatorChangedByUser.get()) {
+                control.setGrainDirection(
+                        (plankHeight > plankWidth)
+                                ? PlankGrainDirection.VERTICAL
+                                : PlankGrainDirection.HORIZONTAL);
+            }
+        };
+
+        Node widthField = createLengthField(control, true);
+        control.plankWidthProperty()
+                .addListener((ob, oldWidth, newWidth) -> {
+                    autoUpdateIndicator.accept(newWidth.intValue(), control.getPlankHeight());
+                });
+
         Label separator = new Label("\u2a09");
 
-        ImageView widthIcon = new ImageView(getClass().getResource("plankWidth.png").toExternalForm());
-        widthIcon.setPreserveRatio(true);
-        widthIcon.fitHeightProperty()
-                .bind(widthField.heightProperty());
+        Node heightField = createLengthField(control, false);
+        control.plankHeightProperty()
+                .addListener((ob, oldHeight, newHeight) -> {
+                    autoUpdateIndicator.accept(control.getPlankWidth(), newHeight.intValue());
+                });
 
-        ImageView heightIcon = new ImageView(getClass().getResource("plankHeight.png").toExternalForm());
-        heightIcon.setPreserveRatio(true);
-        heightIcon.fitHeightProperty()
-                .bind(heightField.heightProperty());
+        Node indicatorNode = createGrainIndicator(control);
 
-        HBox contentRow = new HBox(widthIcon, widthField, separator, heightIcon, heightField, indicator);
+        HBox contentRow = new HBox(widthField, separator, heightField, indicatorNode);
         contentRow.setAlignment(Pos.CENTER_LEFT);
         contentRow.setSpacing(5);
         getChildren()
