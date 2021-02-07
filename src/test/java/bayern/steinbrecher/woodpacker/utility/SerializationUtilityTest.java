1fb49211f57b40628e03c09a98ff35b2c2b00d1a
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-02-07 09:22:22 +0100

    Add unit test setup

---
+++ src/test/java/bayern/steinbrecher/woodpacker/utility/SerializationUtilityTest.java
@@ -0,0 +1,14 @@
+package bayern.steinbrecher.woodpacker.utility;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Stefan Huber
+ * @since 0.1
+ */
+public class SerializationUtilityTest {
+    @Test
+    void emptyTest() {
+        System.out.println(42);
+    }
+}
