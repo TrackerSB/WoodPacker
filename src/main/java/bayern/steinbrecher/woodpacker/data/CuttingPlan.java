d184e3b8d4b3694383edad83c9af7a9aa91ee114
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-02-26 12:03:46 +0100

    Add missing author and version information

--- src/main/java/bayern/steinbrecher/woodpacker/data/CuttingPlan.java
+++ src/main/java/bayern/steinbrecher/woodpacker/data/CuttingPlan.java
@@ -3,6 +3,10 @@ package bayern.steinbrecher.woodpacker.data;
 import java.util.Collection;
 import java.util.Collections;
 
+/**
+ * @author Stefan Huber
+ * @since 0.1
+ */
 public class CuttingPlan {
     private final Collection<PlankSolutionRow> rows;
 
