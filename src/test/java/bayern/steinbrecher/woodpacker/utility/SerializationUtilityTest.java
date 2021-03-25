63cc26b8e035b3c5770266211d11aca7092c9ae5
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-03-25 11:06:46 +0100

    Avoid unreliable anonymous subclasses for serialization tests and test plank problem internal serial version 2

--- src/test/java/bayern/steinbrecher/woodpacker/utility/SerializationUtilityTest.java
+++ src/test/java/bayern/steinbrecher/woodpacker/utility/SerializationUtilityTest.java
@@ -1,7 +1,6 @@
 package bayern.steinbrecher.woodpacker.utility;
 
 import bayern.steinbrecher.woodpacker.data.BasePlank;
-import bayern.steinbrecher.woodpacker.data.Plank;
 import bayern.steinbrecher.woodpacker.data.PlankGrainDirection;
 import bayern.steinbrecher.woodpacker.data.PlankMaterial;
 import bayern.steinbrecher.woodpacker.data.PlankProblem;
@@ -19,8 +18,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
+import java.util.OptionalLong;
 
 /**
  * @author Stefan Huber
@@ -31,82 +31,92 @@ import java.util.Set;
 public final class SerializationUtilityTest {
     private static final String REFERENCE_FILE_PATTERN = "serialized%s%d.bin";
 
-    @SuppressWarnings("unchecked")
     private <C extends Serializable> void checkSerializationForClass(
-            final Set<Long> versions, final C reference, final Class<C> typeDummy, final String... methodsToIgnore)
+            final Map<Long, C> references, final Class<C> typeDummy, final String... methodsToIgnore)
             throws URISyntaxException, IOException, ClassNotFoundException {
         List<String> failMessages = new ArrayList<>();
 
         // Check given versions
-        for (final long version : versions) {
-            final String referenceFileName = String.format(REFERENCE_FILE_PATTERN, typeDummy.getSimpleName(), version);
+        for (Map.Entry<Long, C> entry : references.entrySet()) {
+            final String referenceFileName
+                    = String.format(REFERENCE_FILE_PATTERN, typeDummy.getSimpleName(), entry.getKey());
             final Path referenceFilePath = Path.of(
                     SerializationUtilityTest.class.getResource(referenceFileName).toURI());
             final byte[] serializedObject = Files.readAllBytes(referenceFilePath);
             final C deserializedObject = SerializationUtility.deserialize(serializedObject);
-            final Optional<String> failMessage
-                    = ComparisonUtility.comparePublicValues(typeDummy, deserializedObject, reference, methodsToIgnore);
+            final Optional<String> failMessage = ComparisonUtility.comparePublicValues(
+                    typeDummy, deserializedObject, entry.getValue(), methodsToIgnore);
             failMessage.ifPresent(failMessages::add);
         }
 
         // Check whether current serialization and deserialization are inverse to each other
-        final byte[] serializedObject = SerializationUtility.serialize(reference);
-        final C deserialized = SerializationUtility.deserialize(serializedObject);
-        ComparisonUtility.comparePublicValues(typeDummy, deserialized, reference, methodsToIgnore)
-                .ifPresent(failMessages::add);
+        final OptionalLong mostRecentVersion = references.keySet()
+                .stream()
+                .mapToLong(l -> l)
+                .max();
+        if (mostRecentVersion.isPresent()) {
+            final C mostRecentReference = references.get(mostRecentVersion.getAsLong());
+            final byte[] serializedObject = SerializationUtility.serialize(mostRecentReference);
+            final C deserialized = SerializationUtility.deserialize(serializedObject);
+            ComparisonUtility.comparePublicValues(typeDummy, deserialized, mostRecentReference, methodsToIgnore)
+                    .ifPresent(failMessages::add);
+        } else {
+            failMessages.add("Could not check most recent version for correct serialization");
+        }
 
         Assert.assertTrue(failMessages.isEmpty(), String.join("\n", failMessages));
     }
 
-    // Serialization of Plank
-    private static final Set<Long> PLANK_SERIAL_VERSIONS = Set.of(1L);
-    // FIXME An anonymous subclass may be a problem with serialization
-    private static final Plank PLANK_REFERENCE = new Plank(
-            "PlankReference", 15, 16, PlankGrainDirection.IRRELEVANT, "anonymous plank") {
-    };
-
     @Test
-    public void checkSerializationForPlanks() throws URISyntaxException, IOException, ClassNotFoundException {
-        checkSerializationForClass(PLANK_SERIAL_VERSIONS, PLANK_REFERENCE, Plank.class);
+    public void checkSerializationForPlanks() {
+        // FIXME An anonymous subclass is a problem with serialization
+        // checkSerializationForClass(Map.of(
+        //         1L, new Plank("PlankReference", 15, 16, PlankGrainDirection.IRRELEVANT, "anonymous plank") {
+        //         }
+        // ), Plank.class);
     }
-
-    // Serialization of BasePlank
-    private static final Set<Long> BASE_PLANK_SERIAL_VERSIONS = Set.of(1L);
-    private static final BasePlank BASE_PLANK_REFERENCE
-            = new BasePlank("BasePlank reference", 86, 42, PlankGrainDirection.IRRELEVANT, PlankMaterial.OAK);
 
     @Test
     public void checkSerializationForBasePlanks() throws URISyntaxException, IOException, ClassNotFoundException {
-        checkSerializationForClass(BASE_PLANK_SERIAL_VERSIONS, BASE_PLANK_REFERENCE, BasePlank.class);
+        checkSerializationForClass(Map.of(
+                1L, new BasePlank("BasePlank reference", 86, 42, PlankGrainDirection.IRRELEVANT, PlankMaterial.OAK)
+        ), BasePlank.class);
     }
-
-    // Serialization of RequiredPlank
-    private static final Set<Long> REQUIRED_PLANK_SERIAL_VERSIONS = Set.of(1L);
-    private static final RequiredPlank REQUIRED_PLANK_REFERENCE = new RequiredPlank(
-            "RequiredPlank reference", 17, 18, PlankGrainDirection.VERTICAL, "some required plank"
-    );
 
     @Test
     public void checkSerializationForRequiredPlanks() throws URISyntaxException, IOException, ClassNotFoundException {
-        checkSerializationForClass(REQUIRED_PLANK_SERIAL_VERSIONS, REQUIRED_PLANK_REFERENCE, RequiredPlank.class);
+        checkSerializationForClass(Map.of(
+                1L, new RequiredPlank(
+                        "RequiredPlank reference", 17, 18, PlankGrainDirection.VERTICAL, "some required plank")
+        ), RequiredPlank.class);
     }
-
-    // Serialization of PlankProblem
-    private static final Set<Long> PLANK_PROBLEM_SERIAL_VERSIONS = Set.of(1L);
-    // FIXME An anonymous subclass may be a problem with serialization
-    private static final PlankProblem PLANK_PROBLEM_REFERENCE = new PlankProblem() {{
-        setBasePlank(BASE_PLANK_REFERENCE);
-        setCriterionWeight(PlankSolutionCriterion.BREATH_DIFFERENCES, 1);
-        setCriterionWeight(PlankSolutionCriterion.NUM_PLANKS, 2);
-        setCriterionWeight(PlankSolutionCriterion.ROW_SPACE_WASTE, 3);
-        setRequiredPlanks(FXCollections.observableSet(
-                new RequiredPlank("first", 11, 12, PlankGrainDirection.VERTICAL, "first comment"),
-                new RequiredPlank("second", 14, 13, PlankGrainDirection.HORIZONTAL, "second comment")));
-    }};
 
     @Test
     public void checkSerializationForPlankProblems() throws URISyntaxException, IOException, ClassNotFoundException {
-        checkSerializationForClass(PLANK_PROBLEM_SERIAL_VERSIONS, PLANK_PROBLEM_REFERENCE, PlankProblem.class,
-                "getProposedSolution");
+        final PlankProblem PLANK_PROBLEM_REFERENCE_V1 = new PlankProblem();
+        PLANK_PROBLEM_REFERENCE_V1.setBasePlank(
+                new BasePlank("BasePlank reference", 86, 42, PlankGrainDirection.IRRELEVANT, PlankMaterial.OAK));
+        PLANK_PROBLEM_REFERENCE_V1.setCriterionWeight(PlankSolutionCriterion.BREATH_DIFFERENCES, 1);
+        PLANK_PROBLEM_REFERENCE_V1.setCriterionWeight(PlankSolutionCriterion.NUM_PLANKS, 2);
+        PLANK_PROBLEM_REFERENCE_V1.setCriterionWeight(PlankSolutionCriterion.ROW_SPACE_WASTE, 3);
+        PLANK_PROBLEM_REFERENCE_V1.setRequiredPlanks(FXCollections.observableSet(
+                new RequiredPlank("first", 11, 12, PlankGrainDirection.VERTICAL, "first comment"),
+                new RequiredPlank("second", 14, 13, PlankGrainDirection.HORIZONTAL, "second comment")));
+
+        final PlankProblem PLANK_PROBLEM_REFERENCE_V2 = new PlankProblem();
+        PLANK_PROBLEM_REFERENCE_V2.setBasePlank(new BasePlank(
+                "BasePlank reference", 86, 42, PlankGrainDirection.IRRELEVANT, PlankMaterial.OAK));
+        PLANK_PROBLEM_REFERENCE_V2.setCriterionWeight(PlankSolutionCriterion.BREATH_DIFFERENCES, 1);
+        PLANK_PROBLEM_REFERENCE_V2.setCriterionWeight(PlankSolutionCriterion.NUM_PLANKS, 2);
+        PLANK_PROBLEM_REFERENCE_V2.setCriterionWeight(PlankSolutionCriterion.ROW_SPACE_WASTE, 3);
+        PLANK_PROBLEM_REFERENCE_V2.setRequiredPlanks(FXCollections.observableSet(
+                new RequiredPlank("first", 11, 12, PlankGrainDirection.VERTICAL, "first comment"),
+                new RequiredPlank("second", 14, 13, PlankGrainDirection.HORIZONTAL, "second comment")));
+        PLANK_PROBLEM_REFERENCE_V2.setCuttingWidth(3);
+
+        checkSerializationForClass(Map.of(
+                1L, PLANK_PROBLEM_REFERENCE_V1,
+                2L, PLANK_PROBLEM_REFERENCE_V2
+        ), PlankProblem.class, "getProposedSolution");
     }
 }
