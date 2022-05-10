TrackerSB/WoodPacker

commit 470b8559f1ef9d40e8870bd24f5e096063f02b3d

Make PDFs monochrome instead of grayscale (#59)

src/main/java/bayern/steinbrecher/woodpacker/utility/DrawActionGenerator.java
@@ -8,13 +8,16 @@ import bayern.steinbrecher.woodpacker.data.PlankSolutionRow;
 import bayern.steinbrecher.woodpacker.data.RequiredPlank;
 import bayern.steinbrecher.woodpacker.elements.ScaledCanvas;
 import bayern.steinbrecher.woodpacker.internal.CompileSettings;
+import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.geometry.VPos;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,13 +36,16 @@ public final class DrawActionGenerator {
     private static final int MIN_NUM_GRAIN_INDICATION_STEPS = 10;
     private static final int MAX_NUM_GRAIN_INDICATION_STEPS = 50;
     private static final int PREFERRED_GRAIN_INDICATION_STEP_SIZE = 30;
+    public static final Color BASE_PLANK_COLOR = Color.BURLYWOOD;
 
     // Required planks drawing action configuration
     /**
-     * The percentage of the width which a label of a required plank is allowed to take.
+     * The percentage of the width/height of a required plank which a label is allowed to take.
      */
     private static final double MAX_LABEL_SIZE_FACTOR = 0.75;
+    private static final double LABEL_SIZE_FACTOR = 0.07;
     private static final double EDGE_BAND_INSET_FACTOR = 0.04;
+    public static final Color REQUIRED_PLANK_COLOR = Color.BURLYWOOD;
 
     private DrawActionGenerator() {
         throw new UnsupportedOperationException("Construction of instances is prohibited");
@@ -97,7 +103,7 @@ public final class DrawActionGenerator {
             drawingActions = gc -> {
                 gc.beginPath();
                 gc.rect(0, 0, basePlank.getWidth(), basePlank.getHeight());
-                gc.setFill(Color.BURLYWOOD);
+                gc.setFill(BASE_PLANK_COLOR);
                 gc.fill();
                 gc.setLineWidth(Math.max(1, maxDimension / 500d));
                 gc.setStroke(Color.BLACK);
@@ -123,6 +129,134 @@ public final class DrawActionGenerator {
         return drawingActions;
     }
 
+    private static double drawEdgeBands(final GraphicsContext context, final RequiredPlank plank,
+                                        final double plankXPos, final double plankYPos) {
+        context.save();
+        context.setStroke(Color.GRAY);
+        final double edgeBandInset = Math.min(plank.getHeight(), plank.getWidth())
+                * EDGE_BAND_INSET_FACTOR;
+        final double minXPos = plankXPos + edgeBandInset;
+        final double maxXPos = plankXPos + plank.getWidth() - edgeBandInset;
+        final double minYPos = plankYPos + edgeBandInset;
+        final double maxYPos = plankYPos + plank.getHeight() - edgeBandInset;
+        for (final EdgeBand edgeBand : plank.getEdgeBands()) {
+            switch (edgeBand) { // NOPMD - 2022-03-19: false-positive SwitchStmtsShouldHaveDefault
+                case LEFT -> context.strokeLine(minXPos, minYPos, minXPos, maxYPos);
+                case UPPER -> context.strokeLine(minXPos, minYPos, maxXPos, minYPos);
+                case RIGHT -> context.strokeLine(maxXPos, minYPos, maxXPos, maxYPos);
+                case LOWER -> context.strokeLine(minXPos, maxYPos, maxXPos, maxYPos);
+            }
+        }
+        context.restore();
+        return edgeBandInset;
+    }
+
+    private static PlankLabelInfo drawPlankLabel(
+            final CuttingPlan cuttingPlan, final GraphicsContext context, final RequiredPlank plank,
+            final double plankXPos, final double plankYPos) {
+        final boolean drawLabelVertical = plank.getHeight() > plank.getWidth();
+        final double maxPossibleLabelLength = drawLabelVertical ? plank.getHeight() : plank.getWidth();
+        final double maxPossibleLabelHeight = drawLabelVertical ? plank.getWidth() : plank.getHeight();
+
+        context.save();
+        context.setTextBaseline(VPos.CENTER);
+        context.setFill(Color.BLACK);
+        final double maxLabelLength = MAX_LABEL_SIZE_FACTOR * maxPossibleLabelLength;
+        final double maxLabelHeight = MAX_LABEL_SIZE_FACTOR * maxPossibleLabelHeight;
+        final double labelFontSize = Math.min(
+                LABEL_SIZE_FACTOR * cuttingPlan.getBasePlank().getHeight(),
+                maxLabelHeight);
+        final Font labelFont = Font.font(labelFontSize);
+        context.setFont(labelFont);
+        final double labelXOffset = plankXPos + (plank.getWidth() / 2d);
+        final double labelYOffset = plankYPos + (plank.getHeight() / 2d);
+        if (drawLabelVertical) {
+            context.translate(labelXOffset, labelYOffset);
+            context.rotate(-90);
+            context.fillText(plank.getPlankId(), 0, 0, maxLabelLength);
+        } else {
+            context.fillText(plank.getPlankId(), labelXOffset, labelYOffset, maxLabelLength);
+        }
+        context.restore();
+
+        return new PlankLabelInfo(plank.getPlankId(), labelFont, maxLabelHeight, maxLabelLength, drawLabelVertical);
+    }
+
+    private static Pair<Double, Double> estimateTextBounds(final String text, final Font font) {
+        final Text textDummy = new Text(text);
+        textDummy.setFont(font);
+        final Bounds textDummyBounds = textDummy.getBoundsInLocal();
+        final double estimatedTextLength = textDummyBounds.getWidth();
+        final double estimatedTextHeight = textDummyBounds.getHeight();
+        return new Pair<>(estimatedTextLength, estimatedTextHeight);
+    }
+
+    private static boolean doDimLabelsIntersect(
+            final double areaWidth, final double areaHeight,
+            final String horizText, final String vertText, final Font font) {
+        final Pair<Double, Double> horizDimLabelBounds = estimateTextBounds(horizText, font);
+        final Pair<Double, Double> vertDimLabelBounds = estimateTextBounds(vertText, font);
+        final boolean horizLabelTooHigh
+                = (areaHeight - vertDimLabelBounds.getKey()) / 2 < horizDimLabelBounds.getValue();
+        final boolean vertLabelTooHigh
+                = (areaWidth - horizDimLabelBounds.getKey()) / 2 < vertDimLabelBounds.getValue();
+        /* NOTE 2022-03-25: If one label is "too high" but the other is not then the area is presumably a longish shape,
+         * thus the labels do not intersect.
+         */
+        return horizLabelTooHigh && vertLabelTooHigh;
+    }
+
+    @SuppressWarnings("PMD.LongVariable")
+    private static void drawDimensioningLabels(final GraphicsContext context, final RequiredPlank plank,
+                                               final double plankXPos, final double plankYPos,
+                                               final double edgeBandInset, final PlankLabelInfo plankLabelInfo) {
+        // Estimate size of plank label
+        final Pair<Double, Double> estimatedLabelBounds = estimateTextBounds(plank.getPlankId(), plankLabelInfo.font());
+
+        // Determine sizes in global Coo
+        final double plankHeight = plank.getHeight();
+        final double plankWidth = plank.getWidth();
+        double estimatedLabelHeight;
+        double estimatedLabelWidth;
+        if (plankLabelInfo.isVertical()) {
+            estimatedLabelHeight = estimatedLabelBounds.getValue();
+            estimatedLabelWidth = estimatedLabelBounds.getKey();
+        } else {
+            estimatedLabelHeight = estimatedLabelBounds.getKey();
+            estimatedLabelWidth = estimatedLabelBounds.getValue();
+        }
+
+        // Determine max font size such that they do not intersect with plank label
+        final double horizDimLabelMaxAvailHeight = (plankHeight - estimatedLabelHeight - 2 * edgeBandInset) / 2;
+        final double vertDimLabelMaxAvailHeight = (plankWidth - estimatedLabelWidth - 2 * edgeBandInset) / 2;
+        final double maxDimLabelFontSize = Math.min(horizDimLabelMaxAvailHeight, vertDimLabelMaxAvailHeight);
+
+        final String horizDimLabelText = String.valueOf(plank.getWidth());
+        final String vertDimLabelText = String.valueOf(plank.getHeight());
+
+        // Determine max font size such that they do not intersect with each other
+        double dimLabelFontSize = maxDimLabelFontSize;
+        while (doDimLabelsIntersect(plankWidth, plankHeight, horizDimLabelText, vertDimLabelText,
+                Font.font(dimLabelFontSize))) {
+            dimLabelFontSize *= 0.9;
+        }
+
+        context.save();
+        context.setFont(Font.font(dimLabelFontSize));
+        context.setTextBaseline(VPos.TOP);
+        context.setFill(Color.BLACK);
+        context.fillText(horizDimLabelText,
+                plankXPos + plank.getWidth() / 2d,
+                plankYPos + edgeBandInset,
+                plank.getWidth());
+        context.rotate(-90);
+        context.fillText(vertDimLabelText,
+                -plankYPos - plank.getHeight() / 2d,
+                plankXPos + edgeBandInset,
+                plank.getHeight());
+        context.restore();
+    }
+
     public static Consumer<GraphicsContext> forCuttingPlan(final CuttingPlan cuttingPlan) {
         Consumer<GraphicsContext> drawingActions;
         if (cuttingPlan.getBasePlank() == null) {
@@ -134,10 +268,12 @@ public final class DrawActionGenerator {
                         .accept(gc);
 
                 gc.setTextAlign(TextAlignment.CENTER);
+
                 for (final PlankSolutionRow row : cuttingPlan.getRows()) {
                     final Point2D rowToBasePlankOffset = row.getStartOffset();
                     double plankToRowXOffset = 0;
                     double plankToRowYOffset = 0;
+
                     for (final RequiredPlank plank : row.getPlanks()) {
                         // Draw plank shape
                         final double plankXPos = rowToBasePlankOffset.getX() + plankToRowXOffset;
@@ -145,73 +281,19 @@ public final class DrawActionGenerator {
 
                         gc.beginPath();
                         gc.rect(plankXPos, plankYPos, plank.getWidth(), plank.getHeight());
-                        gc.setFill(Color.BURLYWOOD);
+                        gc.setFill(REQUIRED_PLANK_COLOR);
                         gc.fill();
                         // NOTE 2021-03-08: Stroke width is set by #forBasePlank(...)
                         gc.setStroke(Color.BLACK);
                         gc.stroke();
                         gc.closePath();
 
-                        // Indicate edge bands
-                        gc.setStroke(Color.GRAY);
-                        final double edgeBandInset = Math.min(plank.getHeight(), plank.getWidth())
-                                * EDGE_BAND_INSET_FACTOR;
-                        final double minXPos = plankXPos + edgeBandInset;
-                        final double maxXPos = plankXPos + plank.getWidth() - edgeBandInset;
-                        final double minYPos = plankYPos + edgeBandInset;
-                        final double maxYPos = plankYPos + plank.getHeight() - edgeBandInset;
-                        for (final EdgeBand edgeBand : plank.getEdgeBands()) {
-                            switch (edgeBand) { // NOPMD - 2022-03-19: false-positive SwitchStmtsShouldHaveDefault
-                                case LEFT -> gc.strokeLine(minXPos, minYPos, minXPos, maxYPos);
-                                case UPPER -> gc.strokeLine(minXPos, minYPos, maxXPos, minYPos);
-                                case RIGHT -> gc.strokeLine(maxXPos, minYPos, maxXPos, maxYPos);
-                                case LOWER -> gc.strokeLine(minXPos, maxYPos, maxXPos, maxYPos);
-                            }
-                        }
-                        gc.setStroke(Color.BLACK);
+                        final double edgeBandInset = drawEdgeBands(gc, plank, plankXPos, plankYPos);
 
-                        final boolean drawLabelVertical = plank.getHeight() > plank.getWidth();
-                        // Size in text direction
-                        final double availableLength = drawLabelVertical ? plank.getHeight() : plank.getWidth();
-                        // Size orthogonal to text direction
-                        final double availableHeight = drawLabelVertical ? plank.getWidth() : plank.getHeight();
+                        final PlankLabelInfo plankLabelInfo
+                                = drawPlankLabel(cuttingPlan, gc, plank, plankXPos, plankYPos);
 
-                        // Draw plank label
-                        gc.setTextBaseline(VPos.CENTER);
-                        gc.setFill(Color.BLACK);
-                        final double maxLabelLength = MAX_LABEL_SIZE_FACTOR * availableLength;
-                        final double maxLabelHeight = MAX_LABEL_SIZE_FACTOR * availableHeight;
-                        final double labelFontSize = Math.min(
-                                cuttingPlan.getBasePlank().getHeight() / 15d,
-                                maxLabelHeight);
-                        gc.setFont(Font.font(labelFontSize));
-                        final double labelXOffset = plankXPos + (plank.getWidth() / 2d);
-                        final double labelYOffset = plankYPos + (plank.getHeight() / 2d);
-                        if (drawLabelVertical) {
-                            gc.translate(labelXOffset, labelYOffset);
-                            gc.rotate(-90);
-                            gc.fillText(plank.getPlankId(), 0, 0, maxLabelLength);
-                            gc.rotate(90);
-                            gc.translate(-labelXOffset, -labelYOffset);
-                        } else {
-                            gc.fillText(plank.getPlankId(), labelXOffset, labelYOffset, maxLabelLength);
-                        }
-
-                        // Draw dimensioning labels
-                        final double dimensioningLabelFontSize
-                                = Math.min(availableLength - maxLabelLength, availableHeight - maxLabelHeight) / 2;
-                        gc.setFont(Font.font(dimensioningLabelFontSize));
-                        gc.setTextBaseline(VPos.TOP);
-                        gc.fillText(String.valueOf(plank.getWidth()),
-                                plankXPos + plank.getWidth() / 2d,
-                                plankYPos + edgeBandInset,
-                                plank.getWidth());
-                        gc.rotate(-90);
-                        gc.fillText(String.valueOf(plank.getHeight()),
-                                -plankYPos - plank.getHeight() / 2d,
-                                plankXPos + edgeBandInset,
-                                plank.getHeight());
-                        gc.rotate(90);
+                        drawDimensioningLabels(gc, plank, plankXPos, plankYPos, edgeBandInset, plankLabelInfo);
 
                         // Move offset to next plank in solution row
                         if (row.isAddingHorizontally()) {
@@ -263,5 +345,14 @@ public final class DrawActionGenerator {
                     canvas.getTheoreticalHeight() / 2,
                     canvas.getTheoreticalWidth());
         };
+    }
+
+    private record PlankLabelInfo(
+            String text,
+            Font font,
+            double maxHeight, // In global Coo
+            double maxWidth, // In global Coo
+            boolean isVertical
+    ) {
     }
 }

src/main/java/bayern/steinbrecher/woodpacker/utility/PDFGenerator.java
@@ -16,7 +16,10 @@ import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.AreaBreakType;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
 import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
@@ -25,6 +28,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.function.Supplier;
 
 /**
  * @author Stefan Huber
@@ -69,29 +73,55 @@ public final class PDFGenerator {
         return list;
     }
 
-    private static Image generateCuttingPlanPage(final WritableImage snapshot) throws IOException {
-        final BufferedImage bufferedSnapshot = SwingFXUtils.fromFXImage(snapshot, null);
-        final boolean rotateVertical = bufferedSnapshot.getWidth() > bufferedSnapshot.getHeight();
-        BufferedImage monochromeSnapshot;
-        if (rotateVertical) {
-            monochromeSnapshot = new BufferedImage(
-                    bufferedSnapshot.getHeight(), bufferedSnapshot.getWidth(), BufferedImage.TYPE_BYTE_BINARY);
-        } else {
-            monochromeSnapshot = new BufferedImage(
-                    bufferedSnapshot.getWidth(), bufferedSnapshot.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+    private static Image generateCuttingPlanPage(final WritableImage cuttingPlan) throws IOException {
+        // Remove wood colored background
+        final PixelReader pixelReader = cuttingPlan.getPixelReader();
+        final PixelWriter pixelWriter = cuttingPlan.getPixelWriter();
+        for (int x = 0; x < cuttingPlan.getWidth(); x++) {
+            for (int y = 0; y < cuttingPlan.getHeight(); y++) {
+                final Color pixelColor = pixelReader.getColor(x, y);
+                if (pixelColor.equals(DrawActionGenerator.BASE_PLANK_COLOR)
+                        || pixelColor.equals(DrawActionGenerator.REQUIRED_PLANK_COLOR)) {
+                    pixelWriter.setColor(x, y, Color.WHITE);
+                }
+            }
         }
-        final Graphics2D monochromeSnapshotGraphics = monochromeSnapshot.createGraphics();
+
+        final boolean rotateVertical = cuttingPlan.getWidth() > cuttingPlan.getHeight();
+
+        // Create output AWT image with modifying properties (including grayscale and eventual rotation)
+        final var modifiedCuttingPlan = ((Supplier<BufferedImage>) () -> {
+            if (rotateVertical) {
+                return new BufferedImage((int) cuttingPlan.getHeight(), (int) cuttingPlan.getWidth(),
+                        BufferedImage.TYPE_USHORT_GRAY);
+            }
+            return new BufferedImage((int) cuttingPlan.getWidth(), (int) cuttingPlan.getHeight(),
+                    BufferedImage.TYPE_USHORT_GRAY);
+        }).get();
+
+        final Graphics2D modifiedCuttingPlanDrawer = modifiedCuttingPlan.createGraphics();
         if (rotateVertical) {
             // Move origin to right upper corner
-            monochromeSnapshotGraphics.translate(
-                    monochromeSnapshot.getWidth() / 2d, monochromeSnapshot.getHeight() / 2d);
-            monochromeSnapshotGraphics.rotate(Math.PI / 2);
-            monochromeSnapshotGraphics.translate(
-                    -monochromeSnapshot.getHeight() / 2d, -monochromeSnapshot.getWidth() / 2d);
+            modifiedCuttingPlanDrawer.translate(
+                    modifiedCuttingPlan.getWidth() / 2d, modifiedCuttingPlan.getHeight() / 2d);
+
+            // Rotate context by 90°
+            modifiedCuttingPlanDrawer.rotate(Math.PI / 2d);
+
+            // Move origin back to center
+            modifiedCuttingPlanDrawer.translate(
+                    -modifiedCuttingPlan.getHeight() / 2d, -modifiedCuttingPlan.getWidth() / 2d);
         }
-        monochromeSnapshotGraphics.drawImage(bufferedSnapshot, 0, 0, null);
+
+        // Convert JavaFX image to AWT image
+        final BufferedImage bufferedCuttingPlan = SwingFXUtils.fromFXImage(cuttingPlan, null);
+
+        // Apply cutting plan to modifying output AWT image
+        modifiedCuttingPlanDrawer.drawImage(bufferedCuttingPlan, 0, 0, null);
+
+        // Convert AWT image to PDF image
         final ByteArrayOutputStream snapshotByteStream = new ByteArrayOutputStream();
-        ImageIO.write(monochromeSnapshot, "png", snapshotByteStream);
+        ImageIO.write(modifiedCuttingPlan, "png", snapshotByteStream);
         return new Image(ImageDataFactory.create(snapshotByteStream.toByteArray()));
     }
 

commit 6ee1b0d748101b251483b9139eb8851350eecfeb

Fix freeze on addition (#88)
Fix issue introduced by 470b8559f1ef9d40e8870bd24f5e096063f02b3d

src/main/java/bayern/steinbrecher/woodpacker/elements/PlankField.java
@@ -40,7 +40,7 @@ public class PlankField<T extends Plank> extends Control implements CheckedContr
     private final StringProperty plankId = new SimpleStringProperty("");
     private final ObjectProperty<Optional<Integer>> plankWidth = new SimpleObjectProperty<>(Optional.empty());
     private final ObjectProperty<Optional<Integer>> plankHeight = new SimpleObjectProperty<>(Optional.empty());
-    private final ObjectProperty<PlankGrainDirection> grainDirection = new SimpleObjectProperty<>();
+    private final ObjectProperty<PlankGrainDirection> grainDirection = new SimpleObjectProperty<>(PlankGrainDirection.IRRELEVANT);
     private final ReadOnlyObjectWrapper<PlankMaterial> material = new ReadOnlyObjectWrapper<>(PlankMaterial.UNDEFINED);
     private final ObjectProperty<PlankMaterial> selectedMaterial = new SimpleObjectProperty<>(PlankMaterial.UNDEFINED);
     private final StringProperty comment = new SimpleStringProperty("");

src/main/java/bayern/steinbrecher/woodpacker/elements/PlankGrainDirectionIndicator.java
@@ -25,6 +25,9 @@ public class PlankGrainDirectionIndicator extends Control {
     public PlankGrainDirectionIndicator(final PlankField<?> autoConnection) {
         super();
         this.autoConnection = autoConnection;
+        if (autoConnection != null) {
+            value.set(autoConnection.getGrainDirection());
+        }
         enableAutoMode();
     }
 

src/main/java/bayern/steinbrecher/woodpacker/utility/DrawActionGenerator.java
@@ -22,6 +22,8 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A utility for generating drawing actions that can be used with {@link ScaledCanvas}. The colors used by any draw
@@ -32,6 +34,8 @@ import java.util.function.Consumer;
  * @since 0.1
  */
 public final class DrawActionGenerator {
+    private static final Logger LOGGER = Logger.getLogger(DrawActionGenerator.class.getName());
+
     // Base planks drawing action configuration
     private static final int MIN_NUM_GRAIN_INDICATION_STEPS = 10;
     private static final int MAX_NUM_GRAIN_INDICATION_STEPS = 50;
@@ -231,30 +235,35 @@ public final class DrawActionGenerator {
         final double vertDimLabelMaxAvailHeight = (plankWidth - estimatedLabelWidth - 2 * edgeBandInset) / 2;
         final double maxDimLabelFontSize = Math.min(horizDimLabelMaxAvailHeight, vertDimLabelMaxAvailHeight);
 
-        final String horizDimLabelText = String.valueOf(plank.getWidth());
-        final String vertDimLabelText = String.valueOf(plank.getHeight());
+        if (maxDimLabelFontSize > 0) {
+            final String horizDimLabelText = String.valueOf(plank.getWidth());
+            final String vertDimLabelText = String.valueOf(plank.getHeight());
 
-        // Determine max font size such that they do not intersect with each other
-        double dimLabelFontSize = maxDimLabelFontSize;
-        while (doDimLabelsIntersect(plankWidth, plankHeight, horizDimLabelText, vertDimLabelText,
-                Font.font(dimLabelFontSize))) {
-            dimLabelFontSize *= 0.9;
+            // Determine max font size such that they do not intersect with each other
+            double dimLabelFontSize = maxDimLabelFontSize;
+            while (doDimLabelsIntersect(plankWidth, plankHeight, horizDimLabelText, vertDimLabelText,
+                    Font.font(dimLabelFontSize))) {
+                dimLabelFontSize *= 0.9;
+            }
+
+            context.save();
+            context.setFont(Font.font(dimLabelFontSize));
+            context.setTextBaseline(VPos.TOP);
+            context.setFill(Color.BLACK);
+            context.fillText(horizDimLabelText,
+                    plankXPos + plank.getWidth() / 2d,
+                    plankYPos + edgeBandInset,
+                    plank.getWidth());
+            context.rotate(-90);
+            context.fillText(vertDimLabelText,
+                    -plankYPos - plank.getHeight() / 2d,
+                    plankXPos + edgeBandInset,
+                    plank.getHeight());
+            context.restore();
+        } else {
+            LOGGER.log(Level.FINE, "There is not enough space for drawing dimension labels onto %s",
+                    new Object[]{plank.getPlankId()});
         }
-
-        context.save();
-        context.setFont(Font.font(dimLabelFontSize));
-        context.setTextBaseline(VPos.TOP);
-        context.setFill(Color.BLACK);
-        context.fillText(horizDimLabelText,
-                plankXPos + plank.getWidth() / 2d,
-                plankYPos + edgeBandInset,
-                plank.getWidth());
-        context.rotate(-90);
-        context.fillText(vertDimLabelText,
-                -plankYPos - plank.getHeight() / 2d,
-                plankXPos + edgeBandInset,
-                plank.getHeight());
-        context.restore();
     }
 
     public static Consumer<GraphicsContext> forCuttingPlan(final CuttingPlan cuttingPlan) {