58fa676be3142e11f85044d5acfb48de3c02d4ca
Stefan Huber <stefan.huber.niedling@outlook.com>
2022-05-12 08:04:30 +0200

    SerializationUtilityTest: Reduce naming bloat

--- src/test/java/bayern/steinbrecher/woodpacker/utility/SerializationUtilityTest.java
+++ src/test/java/bayern/steinbrecher/woodpacker/utility/SerializationUtilityTest.java
@@ -101,30 +101,30 @@ public final class SerializationUtilityTest {
 
     @Test
     public void checkSerializationForPlankProblems() throws URISyntaxException, IOException, ClassNotFoundException {
-        final PlankProblem PLANK_PROBLEM_REFERENCE_V1 = new PlankProblem();
-        PLANK_PROBLEM_REFERENCE_V1.setBasePlank(
+        final PlankProblem PLANK_PROBLEM_V1 = new PlankProblem();
+        PLANK_PROBLEM_V1.setBasePlank(
                 new BasePlank("BasePlank reference", 86, 42, PlankGrainDirection.IRRELEVANT, PlankMaterial.OAK));
-        PLANK_PROBLEM_REFERENCE_V1.setCriterionWeight(PlankSolutionCriterion.BREATH_DIFFERENCES, 1);
-        PLANK_PROBLEM_REFERENCE_V1.setCriterionWeight(PlankSolutionCriterion.NUM_PLANKS, 2);
-        PLANK_PROBLEM_REFERENCE_V1.setCriterionWeight(PlankSolutionCriterion.ROW_SPACE_WASTE, 3);
-        PLANK_PROBLEM_REFERENCE_V1.setRequiredPlanks(FXCollections.observableSet(
+        PLANK_PROBLEM_V1.setCriterionWeight(PlankSolutionCriterion.BREATH_DIFFERENCES, 1);
+        PLANK_PROBLEM_V1.setCriterionWeight(PlankSolutionCriterion.NUM_PLANKS, 2);
+        PLANK_PROBLEM_V1.setCriterionWeight(PlankSolutionCriterion.ROW_SPACE_WASTE, 3);
+        PLANK_PROBLEM_V1.setRequiredPlanks(FXCollections.observableSet(
                 new RequiredPlank("first", 11, 12, PlankGrainDirection.VERTICAL, "first comment"),
                 new RequiredPlank("second", 14, 13, PlankGrainDirection.HORIZONTAL, "second comment")));
 
-        final PlankProblem PLANK_PROBLEM_REFERENCE_V2 = new PlankProblem();
-        PLANK_PROBLEM_REFERENCE_V2.setBasePlank(new BasePlank(
+        final PlankProblem PLANK_PROBLEM_V2 = new PlankProblem();
+        PLANK_PROBLEM_V2.setBasePlank(new BasePlank(
                 "BasePlank reference", 86, 42, PlankGrainDirection.IRRELEVANT, PlankMaterial.OAK));
-        PLANK_PROBLEM_REFERENCE_V2.setCriterionWeight(PlankSolutionCriterion.BREATH_DIFFERENCES, 1);
-        PLANK_PROBLEM_REFERENCE_V2.setCriterionWeight(PlankSolutionCriterion.NUM_PLANKS, 2);
-        PLANK_PROBLEM_REFERENCE_V2.setCriterionWeight(PlankSolutionCriterion.ROW_SPACE_WASTE, 3);
-        PLANK_PROBLEM_REFERENCE_V2.setRequiredPlanks(FXCollections.observableSet(
+        PLANK_PROBLEM_V2.setCriterionWeight(PlankSolutionCriterion.BREATH_DIFFERENCES, 1);
+        PLANK_PROBLEM_V2.setCriterionWeight(PlankSolutionCriterion.NUM_PLANKS, 2);
+        PLANK_PROBLEM_V2.setCriterionWeight(PlankSolutionCriterion.ROW_SPACE_WASTE, 3);
+        PLANK_PROBLEM_V2.setRequiredPlanks(FXCollections.observableSet(
                 new RequiredPlank("first", 11, 12, PlankGrainDirection.VERTICAL, "first comment"),
                 new RequiredPlank("second", 14, 13, PlankGrainDirection.HORIZONTAL, "second comment")));
-        PLANK_PROBLEM_REFERENCE_V2.setCuttingWidth(3);
+        PLANK_PROBLEM_V2.setCuttingWidth(3);
 
         checkSerializationForClass(Map.of(
-                1L, PLANK_PROBLEM_REFERENCE_V1,
-                2L, PLANK_PROBLEM_REFERENCE_V2
+                1L, PLANK_PROBLEM_V1,
+                2L, PLANK_PROBLEM_V2
         ), PlankProblem.class, "getProposedSolution");
     }
 }
